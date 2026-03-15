cc7288243af28d7ad98e66740d3fcffbc2936921
GadipeSuryaKiran <gadipesuryakiran21aiml@student.vardhaman.org>
2026-03-15 21:55:16 +0530

    Added WEEK-04-DAY-04

---
+++ WEEK - 4/DAY-04/CountVowels-Example.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static int CountVowels(string text)
+        {
+            int count = 0;
+            foreach (char ch in text.ToLower())
+            {
+                if ("aeiouAEIOU".Contains(ch))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        static void Main()
+        {
+            string input = "Programming";
+            int result = CountVowels(input);
+            Console.WriteLine("Number of vowels: " + result);
+            Console.ReadLine();
+        }
+    }
+}
