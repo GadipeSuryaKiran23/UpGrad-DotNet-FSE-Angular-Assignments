b2894fe6d3883893bbc9f146cc8074b2a3295429
GadipeSuryaKiran <gadipesuryakiran21aiml@student.vardhaman.org>
2026-03-15 21:57:23 +0530

    Added WEEK-04-DAY-05

---
+++ WEEK - 4/DAY-05/BankAccounts-Case-Study.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main()
+        {
+            var acc = new BankAccount("LT987654", "Jonas", 1200m);
+
+            Console.WriteLine($"Account: {acc.AccountNumber}");
+            Console.WriteLine($"Holder: {acc.AccountHolder}");
+            Console.WriteLine($"Balance: {acc.Balance}");
+
+            acc.Deposit(300);
+
+            bool success = acc.Withdraw(2000); // false
+            Console.WriteLine($"Withdraw success: {success}");
+
+            acc.Withdraw(800); // true
+
+            Console.WriteLine($"Final Balance: {acc.Balance}");
+
+            acc.AccountHolder = "Jonas Jonaitis";
+            Console.WriteLine($"Updated Holder: {acc.AccountHolder}");
+        }
+    }
+}
---
+++ WEEK - 4/DAY-05/Case-Study-@2-Employee Profile in a Company HR System.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{ 
+    class Program
+        {   static void Main()
+            {
+                var emp = new Employee("Marko Horvat", 4500m, 35);
+
+                Console.WriteLine($"Employee ID: {emp.EmployeeId}");
+                Console.WriteLine($"Name: {emp.FullName}");
+                Console.WriteLine($"Salary: {emp.Salary}");
+                Console.WriteLine($"Age: {emp.Age}");
+
+                emp.GiveRaise(15);
+
+                emp.DeductPenalty(500);
+
+                emp.FullName = "Marko Horvat Jr.";
+
+                Console.WriteLine($"Updated Name: {emp.FullName}");
+            }
+        }
+}
